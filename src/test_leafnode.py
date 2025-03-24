320c0c120cc55cfd433e75a982a8fd58d7ea2dce
Philip Alegret <philip.alegret@gmail.com>
2025-03-24 07:45:26 -0400

    Implemented LeafNode class and tests.

---
+++ src/test_leafnode.py
@@ -0,0 +1,41 @@
+import unittest
+from leafnode import LeafNode
+
+
+class TestLeafNode(unittest.TestCase):
+    def test_leaf_to_html_p(self):
+        expected = "<p>Hello, world!</p>"
+
+        actual = LeafNode(
+            tag="p", 
+            value="Hello, world!",
+            props=None
+        ).to_html()
+
+        self.assertEqual(expected, actual)
+
+    def test_leaf_to_html_a(self):
+        expected = '<a href="https://www.google.com">Click me!</a>'
+
+        actual = LeafNode(
+            tag="a", 
+            value="Click me!", 
+            props={ "href": "https://www.google.com" }
+        ).to_html()
+
+        self.assertEqual(expected, actual)
+
+    def test_leaf_to_html_no_tag(self):
+        expected = "Hello, world!"
+
+        actual = LeafNode(
+            tag=None, 
+            value="Hello, world!",
+            props=None 
+        ).to_html()
+
+        self.assertEqual(expected, actual)
+
+
+if __name__ == "__main__":
+    unittest.main()
