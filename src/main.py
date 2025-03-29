74dbbe35487bcc22552f90c45c80937043c5f29d
Philip Alegret <philip.alegret@gmail.com>
2025-03-29 10:53:26 -0400

    Added publish function.

--- src/main.py
+++ src/main.py
@@ -1,15 +1,38 @@
-from pprint import pprint
+import os
+import shutil
 
-from models.textnode import TextNode
-from models.texttype import TextType
+
+def publish():
+    source_directory = "static"
+    destination_directory = "public"
+
+    if not os.path.exists(source_directory):
+        print(f"Source directory '{source_directory}' does not exist.")
+        return
+
+    if os.path.exists(destination_directory):
+        shutil.rmtree(destination_directory)
+        print(f"Removed existing destination directory '{destination_directory}'.")
+
+    os.makedirs(destination_directory)
+    print(f"Created destination directory '{destination_directory}'.")
+
+    for root, dirs, files in os.walk(source_directory):
+        dest_dir = os.path.join(destination_directory, os.path.relpath(root, source_directory))
+        os.makedirs(dest_dir, exist_ok=True)
+        print(f"Created directory '{dest_dir}'.")
+
+        for file in files:
+            src_file = os.path.join(root, file)
+            dest_file = os.path.join(dest_dir, file)
+            shutil.copy(src_file, dest_file)
+            print(f"Copied '{src_file}' to '{dest_file}'.")
 
 
 def main():
-    pprint(TextNode(
-        text="This is some anchor text", 
-        text_type=TextType.LINK, 
-        url="https://www.boot.dev/"
-    ))
+    print("Starting the Static Site Generator...")
+    print("Publishing static files...")
+    publish()
     
 
 main()
