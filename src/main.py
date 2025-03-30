ea5c40b191e7dc827d768f2639c9ccc144c42849
Philip Alegret <philip.alegret@gmail.com>
2025-03-30 05:43:47 -0400

    Refactored publishing code.

--- src/main.py
+++ src/main.py
@@ -1,38 +1,9 @@
-import os
-import shutil
-
-
-def publish():
-    source_directory = "static"
-    destination_directory = "public"
-
-    if not os.path.exists(source_directory):
-        print(f"Source directory '{source_directory}' does not exist.")
-        return
-
-    if os.path.exists(destination_directory):
-        shutil.rmtree(destination_directory)
-        print(f"Removed existing destination directory '{destination_directory}'.")
-
-    os.makedirs(destination_directory)
-    print(f"Created destination directory '{destination_directory}'.")
-
-    for root, dirs, files in os.walk(source_directory):
-        dest_dir = os.path.join(destination_directory, os.path.relpath(root, source_directory))
-        os.makedirs(dest_dir, exist_ok=True)
-        print(f"Created directory '{dest_dir}'.")
-
-        for file in files:
-            src_file = os.path.join(root, file)
-            dest_file = os.path.join(dest_dir, file)
-            shutil.copy(src_file, dest_file)
-            print(f"Copied '{src_file}' to '{dest_file}'.")
+from publish import publish_static_content
 
 
 def main():
     print("Starting the Static Site Generator...")
-    print("Publishing static files...")
-    publish()
+    publish_static_content(source_directory="static", destination_directory = "public")
     
 
 main()
